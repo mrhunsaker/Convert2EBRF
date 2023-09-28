92553b3acc786694268a2d0ac005307be776418c
Michael Whapples <mwhapples@aim.com>
2023-09-28 11:41:39 +0100

    Fix a warning.

--- src/convert2ebrf/__main__.py
+++ src/convert2ebrf/__main__.py
@@ -8,12 +8,16 @@ from __feature__ import snake_case, true_property
 from convert2ebrf.brf2ebrf import Brf2EbrfWidget
 
 
-def main(args: Sequence[str] = sys.argv):
+def run_app(args: Sequence[str]):
     app = QApplication(args)
     w = Brf2EbrfWidget()
     w.show()
     app.exec()
 
 
+def main():
+    run_app(sys.argv)
+
+
 if __name__ == "__main__":
-    main(sys.argv)
+    main()
