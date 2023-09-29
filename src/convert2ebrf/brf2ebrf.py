fef01c2586ad610ef24b3458cb0364c295ebbbc6
Michael Whapples <mwhapples@aim.com>
2023-09-29 14:47:13 +0100

    Add properties for the form fields so they can be retrieved by the dialog.

--- src/convert2ebrf/brf2ebrf.py
+++ src/convert2ebrf/brf2ebrf.py
@@ -9,18 +9,44 @@ class Brf2EbrfWidget(QWidget):
     def __init__(self, parent: QObject = None):
         super().__init__(parent)
         layout = QFormLayout()
-        layout.add_row("Input BRF", QLineEdit(self))
-        self.include_images_checkbox = QCheckBox(self)
-        layout.add_row("Include images", self.include_images_checkbox)
-        self.image_dir_edit = QLineEdit(self)
-        layout.add_row("Image directory", self.image_dir_edit)
-        layout.add_row("Output EBRF", QLineEdit(self))
+        self._input_brf_edit = QLineEdit(self)
+        layout.add_row("Input BRF", self._input_brf_edit)
+        self._include_images_checkbox = QCheckBox(self)
+        layout.add_row("Include images", self._include_images_checkbox)
+        self._image_dir_edit = QLineEdit(self)
+        layout.add_row("Image directory", self._image_dir_edit)
+        self._output_ebrf_edit = QLineEdit(self)
+        layout.add_row("Output EBRF", self._output_ebrf_edit)
         self.set_layout(layout)
         self.update_include_images_state()
-        self.include_images_checkbox.stateChanged.connect(self.update_include_images_state)
+        self._include_images_checkbox.stateChanged.connect(self.update_include_images_state)
 
     def update_include_images_state(self):
-        self.image_dir_edit.enabled = self.include_images_checkbox.checked
+        self._image_dir_edit.enabled = self._include_images_checkbox.checked
+    @property
+    def input_brf(self) -> str:
+        return self._input_brf_edit.text
+    @input_brf.setter
+    def set_input_brf(self, value: str):
+        self._input_brf_edit.text = value
+    @property
+    def include_images(self) -> bool:
+        return self._include_images_checkbox.checked
+    @include_images.setter
+    def set_include_images(self, value: bool):
+        self._include_images_checkbox.checked = value
+    @property
+    def image_directory(self) -> str:
+        return self._image_dir_edit.text
+    @image_directory.setter
+    def set_image_directory(self, value: str):
+        self._image_dir_edit.text = value
+    @property
+    def output_ebrf(self) -> str:
+        return self._output_ebrf_edit.text
+    @output_ebrf.setter
+    def set_output_ebrf(self, value: str):
+        self._output_ebrf_edit.text = value
 
 
 class Brf2EbrfDialog(QDialog):
@@ -28,7 +54,8 @@ class Brf2EbrfDialog(QDialog):
         super().__init__(parent)
         self.window_title = "Convert BRF to EBRF"
         layout = QVBoxLayout()
-        layout.add_widget(Brf2EbrfWidget())
+        self._brf2ebrf_form = Brf2EbrfWidget()
+        layout.add_widget(self._brf2ebrf_form)
         self.button_box = QDialogButtonBox(QDialogButtonBox.StandardButton.Close)
         convert_button = self.button_box.add_button("Convert", QDialogButtonBox.ButtonRole.ApplyRole)
         layout.add_widget(self.button_box)
@@ -39,6 +66,6 @@ class Brf2EbrfDialog(QDialog):
     def on_apply(self):
         dlg = QMessageBox(self)
         dlg.window_title = "Converting to EBRF"
-        dlg.text = "You would have started a conversion to EBRF"
+        dlg.text = f"You would be converting {self._brf2ebrf_form.input_brf} to {self._brf2ebrf_form.output_ebrf}"
         dlg.icon = QMessageBox.Icon.Information
         dlg.exec()
