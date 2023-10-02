80caf2d3f5a46b15cd7790b609832b1d80b0b18a
Michael Whapples <mwhapples@aim.com>
2023-10-02 16:22:10 +0100

    Add a helper class for allowing to use a python callable as a QRunnable

---
+++ src/convert2ebrf/utils.py
@@ -0,0 +1,12 @@
+from PySide6.QtCore import QRunnable
+
+
+class RunnableAdapter(QRunnable):
+    def __init__(self, fn, *args, **kwargs):
+        super().__init__()
+        self.fn = fn
+        self.args = args
+        self.kwargs = kwargs
+
+    def run(self):
+        self.fn(*self.args, **self.kwargs)
